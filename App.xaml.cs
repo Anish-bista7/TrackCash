3eed61674c1f799bb33e3ed94ba739ad9edcdac7
Anish Bista <bistaanish89@gmail.com>
2025-01-05 10:58:25 +0545

    Initial commit for TrackCash project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TrackCash
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
